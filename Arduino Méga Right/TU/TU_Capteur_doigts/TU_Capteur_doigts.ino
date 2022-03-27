e372e4d2c41447c0319ceb79a40a7e02f8799483
QGim <qgimonnet@gmail.com>
2022-03-27 19:48:39 +0200

    modif vitesse serial

--- Arduino Méga Right/TU/TU_Capteur_doigts/TU_Capteur_doigts.ino
+++ Arduino Méga Right/TU/TU_Capteur_doigts/TU_Capteur_doigts.ino
@@ -8,17 +8,12 @@ Annu  : A11
 Auri  : A10
 */
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
   int sensorValue = analogRead(A14);
-  // print out the value you read:
   Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+  delay(1);        
 }
